14e3ccc3eba4c2ef35403c623ab9c4c90f0043d5
Gilberto Vargas <tachoguitar@gmail.com>
2020-09-03 22:22:04 -0500

    Fix some validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
-  validates :shortname, presence: true, length: { minimum: 3, maximum: 8 }
+  validates :shortname, presence: true, length: { minimum: 3, maximum: 32 }
   validates :fullname, presence: true, length: { minimum: 3, maximum: 128 }
 
   belongs_to :organization
